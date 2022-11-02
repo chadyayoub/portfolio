21e62c7ddc027433d40ed8d14a39c2d35dff47ac
Chady Ayoub <chadyayoub@mac-m1.local>
2022-11-02 19:18:51 +0200

    github commit

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,9 +1,62 @@
-import React from "react";
-import Text from "../../components/atoms/text/text";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import "./contact.css";
 
-const Contact = () => (
-  <div>
-    <Text>This is the about page</Text>
-  </div>
-);
+const Contact = () => {
+  console.log("sending");
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+    console.log(form.current);
+    emailjs
+      .sendForm(
+        "service_lm230to",
+        "template_aok10o9",
+        form.current,
+        "6pkqKXDtshJ9b2vZJ"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+  return (
+    <div id="contactContainer">
+      <div id="contactform">
+        <p id="leaveAMessage">Leave a message</p>
+        <div id="formContainer">
+          <form ref={form} onSubmit={sendEmail}>
+            <div className="inputContainer">
+              <label className="fieldText">Mail</label>
+              <input
+                className="inputField"
+                placeholder="Mail"
+                name="from_mail"
+              />
+            </div>
+            <div className="inputContainer">
+              <label className="fieldText">Name</label>
+              <input
+                className="inputField"
+                placeholder="Name"
+                name="from_name"
+              />
+            </div>
+            <div className="inputContainer">
+              <label className="fieldText">Message</label>
+              <textarea id="message" name="message" placeholder="Message" />
+            </div>
+            <div id="buttonContainer">
+              <input id="submit" type="submit" value="Send" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Contact;
--- src/pages/landingPage/landingPage.js
+++ src/pages/landingPage/landingPage.js
@@ -1,25 +1,118 @@
-import React from "react";
-import Text from "../../components/atoms/text/text";
+import React, { useCallback } from "react";
 import "./landingPage.css";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 
-const LandingPage = ({ style }) => (
-  <div id="landingContainer">
-    <div id="homeLeftContent">
-      <p className="leftContentTitle">Ohayo sekai good morning world!</p>
-      <p className="leftContentBody">
-        Thank you for visiting my website! I am a developer based in lebanon,
-        and i've worked on different types of software. My main skills are
-        developing mobile apps natively in android and cross platform in react
-        native. I also do web apps using plain html/css/js or using react, and
-        also worked on some game projects for fun using unity engine.
-      </p>
-      <p id="button">
-        <span>My work </span>
-      </p>
-    </div>
-    <div id="imageContainer">
-      <img id="image" src={require("../../assets/naruto.png")} alt="me" />
-    </div>
-  </div>
-);
+const LandingPage = ({ style }) => {
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
+  return (
+    <>
+      <div id="landingContainer">
+        <div id="homeLeftContent">
+          <p className="leftContentTitle">Ohayo sekai good morning world!</p>
+          <p className="leftContentBody">
+            Thank you for visiting my website! I am a developer based in
+            lebanon, and i've worked on different types of software. My main
+            skills are developing mobile apps natively in android and cross
+            platform in react native. I also do web apps using plain html/css/js
+            or using react, and also worked on some game projects for fun using
+            unity engine.
+          </p>
+          <p id="button">
+            <span>My work </span>
+          </p>
+        </div>
+        <div id="imageContainer">
+          <img id="image" src={require("../../assets/naruto.png")} alt="me" />
+        </div>
+      </div>
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        options={{
+          fullScreen: {
+            enable: true,
+            zIndex: 0,
+          },
+          particles: {
+            number: {
+              value: 15,
+            },
+            color: {
+              value: "#fff",
+            },
+            shape: {
+              type: "circle",
+              options: {
+                sides: 5,
+              },
+            },
+            opacity: {
+              value: 0.8,
+              random: false,
+              anim: {
+                enable: false,
+                speed: 1,
+                opacity_min: 0.1,
+                sync: false,
+              },
+            },
+            size: {
+              value: 4,
+              random: true,
+              anim: {
+                enable: false,
+                speed: 40,
+                size_min: 0.1,
+                sync: false,
+              },
+            },
+            rotate: {
+              value: 0,
+              random: true,
+              direction: "clockwise",
+              animation: {
+                enable: true,
+                speed: 5,
+                sync: false,
+              },
+            },
+
+            move: {
+              enable: true,
+              speed: 2,
+              direction: "none",
+              random: false,
+              straight: false,
+              out_mode: "out",
+              attract: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 1200,
+              },
+            },
+          },
+
+          retina_detect: true,
+          background: {
+            image: "",
+            position: "50% 50%",
+            repeat: "no-repeat",
+            size: "cover",
+          },
+        }}
+      />
+    </>
+  );
+};
 export default LandingPage;

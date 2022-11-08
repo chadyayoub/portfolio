066cb409f84733b5193fcc6cd5f2ce0dbb31feb9
Chady Ayoub <chadyayoub@mac-m1.local>
2022-11-08 17:13:42 +0200

    finished contact me form page

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,12 +1,22 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 import "./contact.css";
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [nameDirty, setNameDirty] = useState(false);
+  const [nameError, setNameError] = useState("");
+  const [message, setMessage] = useState("");
+  const [messageDirty, setMessageDirty] = useState(false);
+  const [messageError, setMessageError] = useState("");
+
   console.log("sending");
   const form = useRef();
   const sendEmail = (e) => {
     e.preventDefault();
+    if (!nameDirty) setNameDirty(true);
+    if (!messageDirty) setMessageDirty(true);
+    if (name === "" || message === "") return;
     emailjs
       .sendForm(
         process.env.REACT_APP_EMAIL_JS_SERVICE_ID,
@@ -23,6 +33,27 @@ const Contact = () => {
         }
       );
   };
+  useEffect(() => {
+    if (name === "") setNameError("Enter your name unless you are a pussy");
+    else if (name.length > 30) setNameError("Your name is not that long baka");
+    else setNameError("");
+  }, [name, nameDirty]);
+
+  useEffect(() => {
+    if (message === "")
+      setMessageError(
+        `Dont send me nothing${name ? " " : ""}${name}, I will kill you`
+      );
+    else if (message.length > 1500)
+      setMessageError(
+        `Unless it is something very 
+        exciting${
+          name ? " " : ""
+        }${name}, I don't think I will read all of that...`
+      );
+    else setMessageError("");
+  }, [message, name]);
+
   return (
     <div id="contactContainer">
       <div id="contactform">
@@ -32,14 +63,37 @@ const Contact = () => {
             <div className="inputContainer">
               <label className="fieldText">Name</label>
               <input
+                value={name}
                 className="inputField"
                 placeholder="Name"
                 name="from_name"
+                onChange={(e) => {
+                  setName(e.target.value);
+                  if (!nameDirty) setNameDirty(true);
+                }}
               />
+              <div className="inputError">{nameDirty ? nameError : " "}</div>
             </div>
             <div className="inputContainer">
               <label className="fieldText">Message</label>
-              <textarea id="message" name="message" placeholder="Message" />
+              <textarea
+                id="message"
+                name="message"
+                placeholder="Message"
+                value={message}
+                onChange={(e) => {
+                  setMessage(e.target.value);
+                  if (!messageDirty) setMessageDirty(true);
+                }}
+              />
+              <div
+                className="inputError"
+                style={{
+                  color: message.length > 1500 ? "yellow" : "rgb(160, 32, 32)",
+                }}
+              >
+                {messageDirty ? messageError : " "}
+              </div>
             </div>
             <div id="buttonContainer">
               <i></i>
